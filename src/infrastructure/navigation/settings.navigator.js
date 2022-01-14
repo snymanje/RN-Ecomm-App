74a8c06faa7c7e3a5e6de4a7c181e819b343274e
Jean Snyman <jeansn@tfg.co.za>
2022-01-14 13:34:38 +0200

    added profile pic via camera

--- src/infrastructure/navigation/settings.navigator.js
+++ src/infrastructure/navigation/settings.navigator.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack';
 import { SettingsScreen } from '../../features/settings/screens/settings.screen';
 import { FavouritesScreen } from '../../features/settings/screens/favourites.screen';
+import { CameraScreen } from '../../features/settings/screens/camera.screen';
 
 const SettingsStack = createStackNavigator();
 
@@ -20,5 +21,6 @@ export const SettingsNavigator = ({ route, navigation }) => (
       component={SettingsScreen}
     />
     <SettingsStack.Screen name="Favourites" component={FavouritesScreen} />
+    <SettingsStack.Screen name="Camera" component={CameraScreen} />
   </SettingsStack.Navigator>
 );
